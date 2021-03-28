c31c0fe2c7ddfbf6888a01cbf8d9ff304b1c7a4b
praveen9812 <54749786+praveen9812@users.noreply.github.com>
2021-03-29 00:37:43 +0530

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,20 @@
+<div class="footer">
+  <div class="inner-footer">
 
+<!--  for company name and description -->
+    <div class="footer-items">
+      <h1>Company Name</h1>
+      <p>Description of any product or motto of the company.</p>
+    </div>
+
+    <!--  for quick links  -->
+    <div class="footer-items">
+      <h3>Quick Links</h3>
+      <div class="border1"></div> <!--for the underline -->
+        <ul>
+          <a href="#"><li>Home</li></a>
+          <a href="#"><li>Search</li></a>
+          <a href="#"><li>Contact</li></a>
+          <a href="#"><li>About</li></a>
+        </ul>
+    </div>
